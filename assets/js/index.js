094f3c96826d8f3dff6d18a1a0cc94589630a1c7
Linqui12 <yuri.work95@gmail.com>
2022-06-14 23:44:11 +0300

    fix group

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,53 +1,73 @@
 'use strict'
+
 class User {
     constructor(name, surname) { this.name = name, this.surname = surname };
-    logName() {
-        console.log(this.name + ' ' + this.surname);
+    get fullName() {
+        return this.name + ' ' + this.surname;
     }
-    validateData(year) {
-        let temp = new Date();
-        if (year > temp.getFullYear()) {
-            return temp.getFullYear();
-        }
-        else return year;
-    }
+
 };
 class Student extends User {
     constructor(name, surname, year) {
         super(name, surname);
-        this.year = super.validateData(year);
+        this.year = year;
     }
-    setYear(year) { this.year = this.validateData(year) }
-    logName(){
-        super.logName()+console.log(this.getCourse());
+    validateData(year) {
+        let temp = new Date();
+        temp=temp.getFullYear();
+        if (year <= this.temp|| year >= (this.temp - 5)) {
+            return true;
+        }
+       
+    }
+    set year(year) {
+        if (this.validateData(year)) {
+
+            this.year = year;
+        }
+        //else  throw new RangeError('Incorrect date')
+    }
+
+
+    showName() {
+        return super.fullName;
     }
     getCourse() {
-        let res = new Date();
-        if (res.getFullYear() - this.year > 5) return 'Finish';
-        else return res.getFullYear() - this.year;
+        let res= new Date();
+        return res.getFullYear() - this.year;
     }
 }
 
-let a = new Student('Mark', "marmoz", 2027);
-let b = new Student('Nick', "Nosk", 2012);
-let c = new Student('Peter', "Parker", 2015);
+
+let b = new Student('Nick', "Nosk", 2019);
+let c = new Student('Peter', "Parker", 2018);
 let d = new Student('Otto', "Octavius", 2017);
-let e = new Student('Ni', "Li", 2019);
-e.logName();
+let e = new Student('Ni', "Li", 2020);
+a.showName();
 
 class Group {
-    constructor(name){this.name=name,this.students=new Array()};
-    pushStudent(student){this.students.push(student)};
-
-    logGroup(){this.students.forEach(element =>element.logName())};        
-    
-        
-    
+    constructor(name) { this.name = name, this.students = new Array() };
+    pushStudent(student) { this.students.push(student) };
+    set name(name) {
+        this._name = name;
+    }
+    get name() {
+        return this._name;
+    }
+    set students(students) {
+        this._students = students;
+    }
+    get students() {
+        return this._students;
+    }
+    showStudents() {
+        this._students.forEach(s => console.log(s.showName()));
+    };
 };
-let group=new Group('Fm');
+let group = new Group('Fm');
 group.pushStudent(a);
 group.pushStudent(b);
 group.pushStudent(c);
 group.pushStudent(d);
 group.pushStudent(e);
-group.logGroup();
+group.showStudents();
