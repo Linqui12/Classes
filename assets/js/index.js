9789c13826f86cc8b37e01c3b9a89afb13b32076
Linqui12 <yuri.work95@gmail.com>
2022-06-15 14:52:19 +0300

    Fix validator

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,43 +7,48 @@ class User {
     }
 
 };
+/**
+ * @param {Date} year
+ */
 class Student extends User {
     constructor(name, surname, year) {
         super(name, surname);
         this.year = year;
     }
     validateData(year) {
-        let temp = new Date();
-        temp=temp.getFullYear();
-        if (year <= this.temp|| year >= (this.temp - 5)) {
+        const temp = new Date().getFullYear();
+       const lim= temp-5;
+        if (year <= temp|| year >= lim) {
             return true;
         }
        
     }
     set year(year) {
-        if (this.validateData(year)) {
+        if (!this.validateData(year)) {
 
-            this.year = year;
+             throw new RangeError('Incorrect date')
         }
-        //else  throw new RangeError('Incorrect date')
+        this.year = year;  
     }
-
+    get year(){
+        return this.year;
+    }
 
     showName() {
-        return super.fullName;
+        return console.log(super.fullName);
     }
     getCourse() {
-        let res= new Date();
+        const res= new Date();
         return res.getFullYear() - this.year;
     }
 }
 
 
-let b = new Student('Nick', "Nosk", 2019);
-let c = new Student('Peter', "Parker", 2018);
-let d = new Student('Otto', "Octavius", 2017);
-let e = new Student('Ni', "Li", 2020);
-a.showName();
+let b = new Student('Nick', 'Nosk', 2019);
+let c = new Student('Peter', 'Parker', 2008);
+let d = new Student('Otto', 'Octavius', 2017);
+let e = new Student('Ni', 'Li', 2025);
+
 
 class Group {
     constructor(name) { this.name = name, this.students = new Array() };
@@ -65,9 +70,9 @@ class Group {
     };
 };
 let group = new Group('Fm');
-group.pushStudent(a);
-group.pushStudent(b);
-group.pushStudent(c);
-group.pushStudent(d);
-group.pushStudent(e);
+
+group.pushStudent();
+group.pushStudent();
+group.pushStudent();
+group.pushStudent();
 group.showStudents();
